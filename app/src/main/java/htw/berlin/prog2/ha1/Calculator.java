fcffb1f6025d931cd4657f4eff75a72a9c7b51ad
Nam Le <NguyenDuc.Le@student.htw-berlin.de>
2025-04-16 14:16:31 +0200

    Erste Fehler gefunden Calculator kann kein Punkt wegmachen nach dem is gleicxh .

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -130,6 +130,5 @@ public class Calculator {
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-        if(screen.endsWith(". ")) screen = screen.substring(0,screen.length()-2);
-    }
+
 }
